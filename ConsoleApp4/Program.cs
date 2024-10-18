e3e811ca0425f6ffa8f2d5d11f01317f7acd8522
Kanan-Q <kananag-bp215@code.edu.az>
2024-10-18 23:23:28 +0400

    Weapon-Task

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,153 @@
+﻿namespace ConsoleApp4;
+using static System.Console;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Weapon weapon = null;
+
+        int capacity, currentBullets;
+        bool Auto;
+        while (true)
+        {
+            WriteLine("Maksimum gulle sayı 30 ola biler 30 u kecme");
+            string Read = ReadLine();
+            if (int.TryParse(Read, out capacity) && capacity <= 30 && capacity >= 1)
+            {
+                break;
+            }
+            else
+            {
+                WriteLine("1 den boyuk ve 30-dan kicik ede daxil ele.");
+            }
+        }
+
+        while (true)
+        {
+            WriteLine("Maqazinde olan gulle sayi: ");
+            string Read = ReadLine();
+            if (int.TryParse(Read, out currentBullets) && currentBullets < capacity)
+            {
+                break;
+            }
+            else
+            {
+                WriteLine("30 dan cox gulle ola bilmez.");
+            }
+        }
+
+        while (true)
+        {
+            WriteLine("Modu seçin:(avtomatikdise (true) yaz tekli olsa (false) yaz)");
+
+            string Read = ReadLine();
+            if (Read.ToLower() == "true")
+            {
+                Auto = true;
+                break;
+            }
+            else if (Read.ToLower() == "false")
+            {
+                Auto = false;
+                break;
+            }
+            else
+            {
+                WriteLine("(True) ve (False) yazmag lazimdi.");
+            }
+
+        }
+
+
+        Weapon weapon1 = new Weapon(capacity, currentBullets, Auto);
+        WriteLine("Atisa hazirdi");
+
+
+        do
+        {
+            WriteLine("0. informasiya ucun:");
+            WriteLine("1. Shoot metodu");
+            WriteLine("2. BulletCount");
+            WriteLine("3. Reload");
+            WriteLine("4. FireMood");
+            WriteLine("5. Exit");
+            int choice = int.Parse(ReadLine());
+            switch (choice)
+            {
+                case 0:
+                    WriteLine("Butun data");
+                    weapon1.ShowAllInfo();
+                    break;
+                case 1:
+                    WriteLine("shoot metodunu ucun");
+                    weapon1.Shoot();
+                    break;
+                case 2:
+                    WriteLine("gulle sayi");
+                    weapon1.GetRemainBulletCount();
+                    break;
+                case 3:
+                    weapon1.Reload();
+                    break;
+                case 4:
+                    WriteLine("Modu true=avtomatik false=tekli");
+                    weapon1.ChangeFireMood();
+                    break;
+                case 5:
+                    WriteLine("program dayansin");
+                    break;
+                case 6:
+
+                    bool edit = true;
+                    do
+                    {
+                        WriteLine("7. Tutumu deyis");
+                        WriteLine("8. Gulle sayini deyis");
+                        WriteLine("9. Geri qayit");
+                        int num = int.Parse(ReadLine());
+
+                        switch (num)
+                        {
+                            case 7:
+                                int newCapacity;
+                                WriteLine("Yeni max gulle sayini daxil et");
+                                if (int.TryParse(ReadLine(), out newCapacity))
+                                {
+                                    weapon1.BulletCapacity = newCapacity;
+                                    WriteLine($"Gulle sayi {newCapacity} olaraq deyisdirildi.");
+                                }
+                                else
+                                {
+                                    WriteLine("Duzgun daxil et.");
+                                }
+                                break;
+                            case 8:
+                                int newBulletCount;
+                                WriteLine("Yeni gulle sayını daxil et:");
+                                if (int.TryParse(ReadLine(), out newBulletCount) && newBulletCount >= 0)
+                                {
+                                    weapon1.BulletCount = newBulletCount;
+                                    WriteLine($"Gulle sayı {newBulletCount} olaraq dəyişdirildi.");
+                                }
+                                else
+                                {
+                                    WriteLine("Duzgun daxil et.");
+                                }
+                                break;
+                            case 9:
+                                edit = false;
+                                break;
+                            default:
+                                WriteLine("Duzgun secim et.");
+                                break;
+                        }
+
+                    } while (edit);
+                    break;
+            }
+
+        } while (true);
+    }
+
+
+}
---
+++ ConsoleApp4/Weapon.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Weapon
+    {
+        private double _bulletCapacity;
+        private double _bulletCount;
+
+        public double BulletCapacity //gulle tutumu
+        {
+            get
+            {
+                return _bulletCapacity;
+            }
+            set
+            {
+                if (value >0 && value < 31)
+                {
+                    _bulletCapacity = value;
+                    Console.WriteLine($"Darag bos deyil:{value} gulle var");
+                }
+                else
+                {
+                    Console.WriteLine("O qeder gulle goturmur");
+                }
+            }
+        }
+        public double BulletCount //daragdaki gulle sayi
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCount = value;
+                    Console.WriteLine("en azi bir denede olsa gulle var");
+                }
+            }
+        }
+        public bool ShootMood { get; set; } //atis modu
+        public Weapon(double bulletCapacity, double bulletCount, bool shootMood)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            ShootMood = shootMood;
+        }
+
+        public double Shoot()
+        {
+
+            if (BulletCount >=0&& ShootMood==true)
+            {
+                if (BulletCount == 0)
+                {
+                    Console.WriteLine("Gulle yoxdu");
+                }
+                BulletCount--;
+                Console.WriteLine($"Maqazinde qalan gullenin sayi {BulletCount}");
+            }
+            else
+            {
+                Console.WriteLine($"Tekli mod oldugu ucun avtomatik bir defe 1 gulle atir,qalan gulle sayi:{BulletCount} olur ve proses dayanir.");
+            }
+
+            return BulletCount;
+        }
+        public void GetRemainBulletCount()
+        {
+            double Minus = BulletCapacity - BulletCount;
+            if (Minus <= 31)
+            {
+                Console.WriteLine($"{Minus} dene gulle doldurmalisan");
+
+            }
+            else
+            {
+                Console.WriteLine("Cox gulle doldurursan olmaz!!!");
+            }
+        }
+
+        public void Reload()
+        {
+            if (BulletCapacity < 30)
+
+            {
+                Console.WriteLine("Gulle 30 dan azdi maqazini deyis zarejat ele atis-a hazir veziyyete getir!");
+            }
+
+            else
+            {
+                Console.WriteLine("Tam doludu atisma baslasin");
+            }
+        }
+
+
+        public void ChangeFireMood()
+        {
+            if (ShootMood == true)
+            {
+                Console.WriteLine("Avtomatik atis modundadi amaki luleyi eriye biler fasileli atis et!");
+            }
+            else
+            {
+                Console.WriteLine("Tekli atis modundadir avtomatin esmesi az olacag yaxsidi.");
+            }
+        }
+
+        public void ShowAllInfo()
+        {
+            Console.WriteLine($"Max gulle Tutumu:{BulletCapacity},Maqazindeki gulle sayi{BulletCount},atis Modu:{ShootMood}");
+        }
+
+    }
+}
